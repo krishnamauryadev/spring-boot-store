10b8e4bd007abc4ddc89bc2ea0859059660a5bef
Krishna Maurya <krishna.maurya.dev@gmail.com>
2026-01-30 18:18:35 +0530

    fix carts table migration

--- src/main/resources/db/migration/V3__create_carts_table.sql
+++ src/main/resources/db/migration/V3__create_carts_table.sql
@@ -1,5 +1,4 @@
-create table carts
-(
-    id  BINARY(16) not null primary key,
-    created_at DATE default CURDATE() not null
+CREATE TABLE carts (
+   id BINARY(16) NOT NULL PRIMARY KEY,
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
